edb19a0b0d841b18060199106d9d87bb2acedc2e
AvazbekYuldashev <telegram.vzlom@gmail.com>
2025-09-12 15:37:30 +0500

    Department User Get

--- src/main/java/api/v1/KPI/Management/System/department/service/admin/DepartmentAdminService.java
+++ src/main/java/api/v1/KPI/Management/System/department/service/admin/DepartmentAdminService.java
@@ -5,7 +5,6 @@ import api.v1.KPI.Management.System.department.dto.DepartmentResponseDTO;
 import api.v1.KPI.Management.System.department.dto.admin.DepartmentAdminCreateDTO;
 import api.v1.KPI.Management.System.department.dto.admin.DepartmentAdminUpdateDTO;
 import api.v1.KPI.Management.System.department.mapper.DepartmentMapper;
-import api.v1.KPI.Management.System.department.repository.DepartmentRepository;
 import api.v1.KPI.Management.System.department.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
@@ -16,8 +15,6 @@ import org.springframework.stereotype.Service;
 public class DepartmentAdminService extends DepartmentService {
     @Autowired
     private DepartmentMapper departmentMapper;
-    @Autowired
-    private DepartmentRepository departmentRepository;
 
     public DepartmentResponseDTO adminCreate(DepartmentAdminCreateDTO dto) {
         return departmentMapper.toResponseDTO(create(departmentMapper.toAdminCreateEntity(dto)));
