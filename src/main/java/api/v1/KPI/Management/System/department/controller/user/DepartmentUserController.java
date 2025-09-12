62df7856a9d8cde6e94d351d07f92a113172a798
AvazbekYuldashev <telegram.vzlom@gmail.com>
2025-09-12 15:19:41 +0500

    Department User

---
+++ src/main/java/api/v1/KPI/Management/System/department/controller/user/DepartmentUserController.java
@@ -0,0 +1,4 @@
+package api.v1.KPI.Management.System.department.controller.user;
+
+public class DepartmentUserController {
+}
---
+++ src/main/java/api/v1/KPI/Management/System/department/service/user/DepartmentUserService.java
@@ -0,0 +1,9 @@
+package api.v1.KPI.Management.System.department.service.user;
+
+import api.v1.KPI.Management.System.department.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentUserService extends DepartmentService {
+
+}
