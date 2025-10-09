b99454f61e0bdd61535c72a8db799eeb04451c1f
AvazbekYuldashev <telegram.vzlom@gmail.com>
2025-10-09 16:12:01 +0500

    Update

---
+++ src/main/java/api/v1/KPI/Management/System/offering/controller/owner/OfferingOwnerController.java
@@ -0,0 +1,4 @@
+package api.v1.KPI.Management.System.offering.controller.owner;
+
+public class OfferingOwnerController {
+}
---
+++ src/main/java/api/v1/KPI/Management/System/offering/service/owner/OfferingOwnerService.java
@@ -0,0 +1,6 @@
+package api.v1.KPI.Management.System.offering.service.owner;
+
+import api.v1.KPI.Management.System.offering.service.OfferingService;
+
+public class OfferingOwnerService extends OfferingService {
+}
